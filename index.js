d4fce94741db8ff74669c813f2b80d8e8e292bc2
Racoonsad <you@example.com>
2023-08-12 03:14:03 -0500

    Agregar funcion al boton "inicio" de la "nav bar"

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ botonLibros.addEventListener('click', () => {
     librosContainer.classList.toggle('mostrar-todos');
     libros.forEach(libro => libro.classList.toggle('libro-activo'));
 });
+// index.js
+const enlaceInicio = document.getElementById('enlace-inicio');
+
+enlaceInicio.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    location.reload(); // Recarga la página
+});
+
+
