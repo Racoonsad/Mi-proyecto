267bb81ec7aefeb24a54b73d3cc7914a9593f95f
Racoonsad <you@example.com>
2023-08-12 16:08:23 -0500

    Adicion de comentrio para el archivo js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // index.js
+
+// Seleccionar el contenedor de libros y los elementos de libro
 const librosContainer = document.querySelector('.libros-container');
 const libros = librosContainer.querySelectorAll('.libro');
+
+// Seleccionar el botón de "Libros" en la barra de navegación
 const botonLibros = document.querySelector('.boton-libros');
 
+// Agregar un evento de clic al botón de "Libros"
 botonLibros.addEventListener('click', () => {
+    // Alternar la clase "mostrar-todos" en el contenedor de libros
     librosContainer.classList.toggle('mostrar-todos');
+
+    // Alternar la clase "libro-activo" en cada elemento de libro
     libros.forEach(libro => libro.classList.toggle('libro-activo'));
 });
-// index.js
+
+
+// Obtener el enlace "Inicio" por su ID
 const enlaceInicio = document.getElementById('enlace-inicio');
 
+// Agregar un evento de clic al enlace "Inicio"
 enlaceInicio.addEventListener('click', (event) => {
-    event.preventDefault(); // Evita que el enlace recargue la página
-    location.reload(); // Recarga la página
+    event.preventDefault(); 
+    location.reload(); // Recargar la página
 });
